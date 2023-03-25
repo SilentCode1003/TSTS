2add5c3df6017d34fb53f6ceab9659506d91323a
Josel Catalan <catalanjosel123@gmail.com>
2023-03-25 13:12:48 +0800

    chore: make chat message more rounded and add more padding on upload icon

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -80,7 +80,7 @@ function Conversation({ chatId }) {
         <form onSubmit={handleSubmit} className="flex gap-4">
           <label
             htmlFor="file"
-            className="p-1 flex justify-center items-center border border-red-400 bg-white text-black cursor-pointer"
+            className="p-2 flex justify-center items-center border border-red-400 bg-white text-black rounded cursor-pointer"
           >
             <input
               type="file"
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import { MdDownload } from 'react-icons/md'
 function Message({ message, type }) {
   return (
     <div
-      className={`max-w-full p-4 rounded ${
+      className={`max-w-full p-4 rounded-lg ${
         message.sender === 'You'
           ? 'ml-auto bg-red-500 text-white'
           : 'mr-auto bg-white border border-red-500'

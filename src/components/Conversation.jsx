87a2985a38c5a07549d7b750c1f7044f83821fd1
Josel Catalan <catalanjosel123@gmail.com>
2023-03-25 10:04:06 +0800

    chore: implement upload function

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,30 +1,64 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import chatData from '../data/chat.json'
 import Message from './Message'
+import { MdFileUpload } from 'react-icons/md'
 
 function Conversation({ chatId }) {
   const [chat, setChat] = useState(chatData)
   const [message, setMessage] = useState('')
+  const [uploadedFile, setUploadedFile] = useState(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (!message) return
 
-    setChat((prev) => [
-      {
-        id: crypto.randomUUID(),
-        message,
-        sender: 'You',
-        createdAt:
-          new Date().toLocaleDateString() +
-          ' ' +
-          new Date().toLocaleTimeString(),
-      },
-      ...prev,
-    ])
+    setChat((prev) => [message, ...prev])
 
     setMessage('')
+  }
+
+  const handleInputChange = (e) => {
+    const message = {
+      id: crypto.randomUUID(),
+      sender: 'You',
+      type: 'TEXT',
+      message: e.target.value,
+      createdAt: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+    }
+
+    setMessage(message)
+  }
+
+  const handleFileChange = (e) => {
+    const validFiles = ['image/jpeg', 'image/png']
+    const file = e.target.files[0]
+    let message
+
+    if (validFiles.includes(file.type)) {
+      message = {
+        id: crypto.randomUUID(),
+        sender: 'You',
+        type: 'IMAGE',
+        src: URL.createObjectURL(file),
+        createdAt: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+      }
+    } else {
+      message = {
+        id: crypto.randomUUID(),
+        sender: 'You',
+        type: 'DOCUMENT',
+        href: URL.createObjectURL(file),
+        fileName: file.name,
+        createdAt: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+      }
+    }
+
+    console.log(e)
+
+    setUploadedFile(file)
+    setChat((prev) => [message, ...prev])
+    setUploadedFile('')
   }
 
   useEffect(() => {
@@ -38,18 +72,32 @@ function Conversation({ chatId }) {
     <>
       <div className="p-4 flex flex-col-reverse gap-4 overflow-x-auto">
         {chat.map((message) => (
-          <Message key={message.id} message={message} />
+          <Message key={message.id} message={message} type={message.type} />
         ))}
       </div>
 
       <div className="p-4 mt-auto bg-red-300">
         <form onSubmit={handleSubmit} className="flex gap-4">
+          <label
+            htmlFor="file"
+            className="p-1 flex justify-center items-center border border-red-400 bg-white text-black cursor-pointer"
+          >
+            <input
+              type="file"
+              name="file"
+              id="file"
+              className="hidden"
+              onChange={handleFileChange}
+            />
+            {/* TODO: Make this an icon */}
+            {uploadedFile?.name || <MdFileUpload className="text-red-500" />}
+          </label>
           <input
             type="text"
             placeholder="Send message..."
             className="w-full px-4 py-2 rounded-md"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            value={message?.message || ''}
+            onChange={handleInputChange}
           />
           <button className="px-4 py-2 bg-red-500 text-white rounded">
             Send
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,6 @@
-function Message({ message }) {
+import { MdDownload } from 'react-icons/md'
+
+function Message({ message, type }) {
   return (
     <div
       className={`max-w-full p-4 rounded ${
@@ -8,7 +10,20 @@ function Message({ message }) {
       }`}
     >
       <div>
-        <p className="mb-2">{message.message}</p>
+        {type === 'TEXT' && <p className="mb-2">{message.message}</p>}
+        {type === 'IMAGE' && <img src={message.src} />}
+        {type === 'DOCUMENT' && (
+          <a
+            href={message.href}
+            className="italic underline underline-offset-2"
+            download
+          >
+            {/* TODO: Add download icon here */}
+            <span>
+              {message.fileName} <MdDownload className="inline-block" />
+            </span>
+          </a>
+        )}
         <p className="italic text-sm">{message.createdAt}</p>
       </div>
     </div>

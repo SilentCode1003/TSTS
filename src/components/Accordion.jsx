f8d251478b0730fb09d6e9bc3c53e9cf2f537ee0
Josel Catalan <catalanjosel123@gmail.com>
2023-03-27 15:33:52 +0800

    chore: create and style knowledge base accordion

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { MdArrowDropDown } from 'react-icons/md'
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index)
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const contentActive = index === activeIndex ? 'block' : 'hidden'
+
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className={`py-4 px-4 flex justify-between bg-neutral-400 text-white border-b-2 border-b-neutral-500 font-bold cursor-pointer`}
+          onClick={() => handleClick(index)}
+        >
+          {item.title}
+          <MdArrowDropDown className="text-2xl" />
+        </div>
+        <div className={`p-4 ${contentActive} bg-neutral-300 text-black`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    )
+  })
+
+  return (
+    <>
+      {items.length < 1 ? (
+        ''
+      ) : (
+        <div className="w-full rounded-md border-2 border-neutral-400 overflow-hidden">
+          {renderedItems}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Accordion
--- src/pages/KnowledgeBase.jsx
+++ src/pages/KnowledgeBase.jsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react'
+import Accordion from '../components/Accordion'
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript library for building user interfaces.',
+  },
+  {
+    title: 'Why use React?',
+    content:
+      'React allows for the creation of reusable UI components, making development faster and more efficient.',
+  },
+  {
+    title: 'How do you use React?',
+    content:
+      'React can be used with JavaScript, TypeScript, and other languages. It can also be used with various other libraries and frameworks, such as Redux and Next.js.',
+  },
+]
+
 function KnowledgeBase() {
-  return <div>KnowledgeBase</div>
+  const [searchedItems, setSearchedItems] = useState(items)
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  useEffect(() => {
+    function filterIt(arr, searchKey) {
+      return arr.filter((obj) =>
+        Object.keys(obj).some((key) => obj[key].includes(searchKey))
+      )
+    }
+
+    const filteredItems = filterIt(items, searchTerm)
+    setSearchedItems(filteredItems)
+  }, [searchTerm])
+
+  return (
+    <div className="p-8 flex flex-col gap-8 items-center">
+      <h1 className="text-2xl md:text-4xl font-bold text-center">
+        Knowledge Base
+      </h1>
+
+      <div className="w-full md:w-1/2 rounded">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Search your issue"
+          className="w-full py-4 px-8 md:text-xl bg-gray-50 border text-gray-900 rounded-md"
+        />
+      </div>
+
+      <Accordion items={searchedItems} />
+    </div>
+  )
 }
 
 export default KnowledgeBase

b87f4e97ecd34021e8849632d80b594d2032bd5c
Josel Catalan <catalanjosel123@gmail.com>
2023-03-28 15:03:39 +0800

    chore: create some content for ticket tracking page

---
+++ src/components/TicketTrackingTable.jsx
@@ -0,0 +1,26 @@
+function TicketTrackingTable() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Ticket No.</th>
+            <th>Client/Store</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Client 1</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Client 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TicketTrackingTable
--- src/pages/TicketTracking.jsx
+++ src/pages/TicketTracking.jsx
@@ -1,5 +1,15 @@
+import TicketTrackingTable from '../components/TicketTrackingTable'
+
 function TicketTracking() {
-  return <div>TicketTracking</div>
+  return (
+    <div className="p-8">
+      <p className="text-2xl md:text-4xl font-bold text-center">
+        Ticket Tracking
+      </p>
+
+      <TicketTrackingTable />
+    </div>
+  )
 }
 
 export default TicketTracking

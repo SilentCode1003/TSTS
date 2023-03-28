69f87dd16afe6a48c77a738ab94a0b543bfb18f6
Josel Catalan <catalanjosel123@gmail.com>
2023-03-28 13:04:27 +0800

    chore: create dummy stats on top of dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,13 +2,34 @@ import Card from '../components/Card'
 import Footer from '../components/Footer'
 import { adminItems } from '../components/NavigationBar'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function Dashboard() {
   const navigate = useNavigate()
+  const [newTicketsCount, setNewTicketsCount] = useState(3)
+  const [urgentTicketsCount, setUrgentTicketsCount] = useState(12)
 
   return (
-    <>
-      <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="p-8">
+      <p className="text-2xl md:text-4xl font-bold text-center">Dashboard</p>
+      <div className="p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card classNames="px-4 py-5 min-h-[150px] hover:shadow-md cursor-pointer">
+          <div className="h-full flex flex-col justify-between truncate">
+            <p className="text-lg">New Tickets</p>
+            <p className="font-bold text-4xl text-right">{newTicketsCount}</p>
+          </div>
+        </Card>
+
+        <Card classNames="px-4 py-5 min-h-[150px] hover:shadow-md cursor-pointer">
+          <div className="h-full flex flex-col justify-between truncate">
+            <p className="text-lg">Urgent Tickets</p>
+            <p className="font-bold text-4xl text-right">
+              {urgentTicketsCount}
+            </p>
+          </div>
+        </Card>
+      </div>
+      <div className="p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {adminItems.map((item) => (
           <Card
             key={item.name}
@@ -23,7 +44,7 @@ function Dashboard() {
       </div>
 
       <Footer />
-    </>
+    </div>
   )
 }
 

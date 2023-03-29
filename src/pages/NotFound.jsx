325d98d43a05ead71dab762a4a63b3fa76e1534c
Josel Catalan <catalanjosel123@gmail.com>
2023-03-29 16:19:00 +0800

    chore: style 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { NavLink } from 'react-router-dom'
+
 function NotFound() {
-  return <div>404 - Page not Found :(</div>
+  return (
+    <div className="p-8 w-screen h-screen grid place-items-center place-content-center gap-8">
+      <h1 className="text-red-500 text-3xl md:text-6xl font-bold text-center">
+        404 Page Not Found
+      </h1>
+      <NavLink to="/" className="underline text-lg md:text-2xl">
+        Back to Home
+      </NavLink>
+    </div>
+  )
 }
 
 export default NotFound

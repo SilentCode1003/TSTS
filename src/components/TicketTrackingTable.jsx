c25a4fd1499bcd0ea3c4a75628b72066b692debb
Josel Catalan <catalanjosel123@gmail.com>
2023-03-29 10:18:07 +0800

    chore: style ticket tracking table

--- src/components/TicketTrackingTable.jsx
+++ src/components/TicketTrackingTable.jsx
@@ -1,27 +1,82 @@
-function TicketTrackingTable({ data }) {
+import { MdSubdirectoryArrowRight } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
+
+function TicketTrackingTable({ tickets }) {
+  const navigate = useNavigate()
+
+  const handleClick = (ticket) => {
+    navigate(`/ticket-tracking/${ticket}`)
+  }
+
   return (
-    <div>
-      {!data ? (
+    <div className="w-full overflow-x-auto">
+      {!tickets ? (
         'No data'
       ) : (
-        <table>
-          <thead>
+        <table className="w-full table-auto">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th>Ticket No.</th>
-              <th>Client/Store</th>
-              <th>Date Created</th>
-              <th>Date Updated</th>
-              <th>Priority</th>
-              <th>Status</th>
-              <th>Due Date</th>
-              <th>Urgency</th>
-              <th>Assigned To</th>
+              <th scope="col" className="px-6 py-3">
+                Ticket No.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Client/Store
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date Created
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date Updated
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Priority
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Due Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Urgency
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Assigned To
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Ticket Details
+              </th>
             </tr>
           </thead>
           <tbody>
-            {data?.map((item) => (
-              <tr>
-                <td>{item.ticketNumber}</td>
+            {tickets.map((ticket) => (
+              <tr
+                key={ticket.ticketNumber}
+                className="border-b border-gray-200"
+              >
+                <td className="p-1 text-center text-xs">
+                  {ticket.ticketNumber}
+                </td>
+                <td className="p-1 text-center text-xs">{ticket.client}</td>
+                <td className="p-1 text-center text-xs">
+                  {ticket.dateCreated}
+                </td>
+                <td className="p-1 text-center text-xs">
+                  {ticket.dateUpdated}
+                </td>
+                <td className="p-1 text-center text-xs">{ticket.priority}</td>
+                <td className="p-1 text-center text-xs">{ticket.status}</td>
+                <td className="p-1 text-center text-xs">{ticket.dueDate}</td>
+                <td className="p-1 text-center text-xs">{ticket.urgency}</td>
+                <td className="p-1 text-center text-xs">{ticket.assignedTo}</td>
+                <td className="p-1 h-full text-center">
+                  <button
+                    onClick={() => handleClick(ticket.ticketNumber)}
+                    className="py-1 px-2 mx-auto bg-teal-500 rounded text-white hover:bg-teal-700 text-xs text-center grid place-content-center"
+                  >
+                    <MdSubdirectoryArrowRight />
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
--- src/pages/TicketTracking.jsx
+++ src/pages/TicketTracking.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import TicketTrackingTable from '../components/TicketTrackingTable'
+import ticketsData from '../data/tickets.json'
 
 function TicketTracking() {
+  const [searchInput, setSearchInput] = useState('')
+  const [tickets, setTickets] = useState(ticketsData)
+
+  const handleSearchChange = (e) => {
+    setSearchInput(e.target.value)
+  }
+
   return (
-    <div className="p-8">
+    <div className="p-8 flex flex-col gap-8">
       <p className="text-2xl md:text-4xl font-bold text-center">
         Ticket Tracking
       </p>
 
-      <TicketTrackingTable data={[]} />
+      <div>
+        <input
+          type="text"
+          id="search"
+          onChange={handleSearchChange}
+          value={searchInput}
+          placeholder="Search"
+          className="py-2 px-4 w-full sm:w-1/2 border bg-gray-100 rounded-sm text-gray-900"
+        />
+      </div>
+
+      <TicketTrackingTable tickets={tickets} />
     </div>
   )
 }

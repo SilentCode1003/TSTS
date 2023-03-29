56ffde10d0db14ba7088dc5f42e567e2e34dd019
Josel Catalan <catalanjosel123@gmail.com>
2023-03-29 10:41:38 +0800

    chore: create debounce hook and implement search functionality in ticket tracking

--- src/pages/TicketTracking.jsx
+++ src/pages/TicketTracking.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TicketTrackingTable from '../components/TicketTrackingTable'
 import ticketsData from '../data/tickets.json'
+import { useDebounce } from '../hooks/useDebounce'
 
 function TicketTracking() {
   const [searchInput, setSearchInput] = useState('')
   const [tickets, setTickets] = useState(ticketsData)
+  const query = useDebounce(searchInput, 500)
 
   const handleSearchChange = (e) => {
     setSearchInput(e.target.value)
   }
+
+  useEffect(() => {
+    let filteredArr
+
+    if (searchInput === '' || !searchInput) {
+      filteredArr = ticketsData
+    }
+
+    filteredArr = ticketsData.filter((obj) => {
+      return Object.values(obj).some((val) => {
+        if (typeof val === 'string') {
+          return val.toLowerCase().includes(searchInput.toLowerCase())
+        } else if (typeof val === 'number') {
+          return val.toString().includes(searchInput)
+        }
+      })
+    })
+
+    setTickets(filteredArr)
+  }, [query])
 
   return (
     <div className="p-8 flex flex-col gap-8">

73c82e01c81168e4c294c23d2948266574bb60e2
Josel Catalan <catalanjosel123@gmail.com>
2023-04-15 10:21:20 +0800

    chore: install and implement react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <p>Hello World</p>
+      {/* LeftBar */}
+      <div></div>
+
+      {/* Content */}
+      <div>
+        {/* TopBar */}
+        <div></div>
+        {/* Outlet */}
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,35 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ProtectedRoute>
+        <App />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 )

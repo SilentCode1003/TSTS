98e95254f5f2a90bb7b0b4388f70563ea55d2939
Josel Catalan <catalanjosel123@gmail.com>
2023-04-17 09:21:00 +0800

    chore: make TopCardsItem component

--- src/components/TopCards.jsx
+++ src/components/TopCards.jsx
@@ -1,45 +1,16 @@
+import { GridItem, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
-import {
-  SimpleGrid,
-  Card,
-  CardHeader,
-  Stack,
-  CardBody,
-  Text,
-  GridItem,
-} from '@chakra-ui/react'
+import TopCardsItem from './TopCardsItem'
 
 const TopCards = () => {
   return (
     <SimpleGrid columns={[1, 2, 4]} spacing="4">
-      <Card>
-        <CardHeader>
-          <Text size="md">New Tickets</Text>
-        </CardHeader>
-        <CardBody noOfLines="1">
-          <Stack>
-            <Text as="b" fontSize="3xl" textAlign="end">
-              3
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
+      <TopCardsItem header="New Tickets">3</TopCardsItem>
 
-      <Card>
-        <CardHeader>
-          <Text size="md">Urgent Tickets</Text>
-        </CardHeader>
-        <CardBody noOfLines="1">
-          <Stack>
-            <Text as="b" fontSize="3xl" textAlign="end">
-              12
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
+      <TopCardsItem header="Urgent Tickets">12</TopCardsItem>
 
       <GridItem colSpan="2" rowSpan="2">
-        <Card h="100%">Bar Graph</Card>
+        <TopCardsItem header="Resolved vs Unresolved Tickets"></TopCardsItem>
       </GridItem>
     </SimpleGrid>
   )
---
+++ src/components/TopCardsItem.jsx
@@ -0,0 +1,21 @@
+import { Card, CardBody, CardHeader, Stack, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const TopCardsItem = ({ header, children }) => {
+  return (
+    <Card>
+      <CardHeader minH="88px">
+        <Text size="md">{header}</Text>
+      </CardHeader>
+      <CardBody noOfLines="1">
+        <Stack>
+          <Text as="b" fontSize="3xl" textAlign="end">
+            {children}
+          </Text>
+        </Stack>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default TopCardsItem

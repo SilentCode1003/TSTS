9db5fa19e5b8b5729e7324b6f9dd66ea534f58fa
Josel Catalan <catalanjosel123@gmail.com>
2023-04-17 15:09:07 +0800

    chore: make NavigationCard component a link

--- src/components/NavigationCard.jsx
+++ src/components/NavigationCard.jsx
@@ -1,29 +1,32 @@
 import { Card, CardBody, CardHeader, Flex } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NavigationCard = ({ item }) => {
   return (
-    <Card
-      _hover={{
-        shadow: 'md',
-      }}
-      role="group"
-      cursor="pointer"
-    >
-      <CardHeader h="70px">{item.name}</CardHeader>
-      <CardBody fontSize="6xl">
-        <Flex
-          alignItems="center"
-          justifyContent="center"
-          color="gray.600"
-          _groupHover={{
-            color: 'purple.400',
-          }}
-          transition="color 250ms ease"
-        >
-          {item.icon}
-        </Flex>
-      </CardBody>
-    </Card>
+    <Link to={item.url}>
+      <Card
+        _hover={{
+          shadow: 'md',
+        }}
+        role="group"
+        cursor="pointer"
+      >
+        <CardHeader h="70px">{item.name}</CardHeader>
+        <CardBody fontSize="6xl">
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            color="gray.600"
+            _groupHover={{
+              color: 'purple.400',
+            }}
+            transition="color 250ms ease"
+          >
+            {item.icon}
+          </Flex>
+        </CardBody>
+      </Card>
+    </Link>
   )
 }

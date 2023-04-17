5760287a587ce8fcedc1339e937239f95b745ca5
Josel Catalan <catalanjosel123@gmail.com>
2023-04-17 13:07:05 +0800

    chore: style NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import { Grid, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <Grid p="4" h="100vh" placeItems="center" bg="purple.400">
+      <Heading textAlign="center" color="white" fontSize={['4xl', null, '6xl']}>
+        Page Not Found
+      </Heading>
+    </Grid>
+  )
 }
 
 export default NotFound

89927a0433d627b18041a965b9973a9da7f1763e
Josel Catalan <catalanjosel123@gmail.com>
2023-04-24 11:47:07 +0800

    chore: add some temporary content on TicketAssignmentTable

--- src/components/TicketAssignmentTable.jsx
+++ src/components/TicketAssignmentTable.jsx
@@ -1,7 +1,41 @@
+import {
+  Box,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
 import React from 'react'
 
 const TicketAssignmentTable = () => {
-  return <div>TicketAssignmentTable</div>
+  return (
+    <TableContainer>
+      <Table variant="striped" colorScheme="purple" size="sm">
+        <Thead>
+          <Tr>
+            <Th>Ticket No.</Th>
+            <Th>Client/Store</Th>
+            <Th>Date Created</Th>
+            <Th>Priority</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          <Tr>
+            <Td>1</Td>
+            <Td>Client 1</Td>
+            <Td>March 24, 2023</Td>
+            <Td>High</Td>
+            <Td>Unresolved</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  )
 }
 
 export default TicketAssignmentTable

8ba8999aa788f39feb9cb68670a6b96fcc3f9a9d
Josel Catalan <catalanjosel123@gmail.com>
2023-05-06 16:48:50 +0800

    chore: make logo a link to home page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
 import { Box, Flex, HStack, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
     <Flex justifyContent="center" alignItems="center">
-      <Image w="100px" src={logo} />
+      <Link to="/">
+        <Image w="100px" src={logo} />
+      </Link>
     </Flex>
   )
 }

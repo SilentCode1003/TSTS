d9b9fab0bd240c6326dc3c42ed9202571fc8c05f
Josel Catalan <catalanjosel123@gmail.com>
2023-05-06 16:52:03 +0800

    fix: do not reset leftbar collapse state on logo click

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import { Box, Flex, HStack, Image } from '@chakra-ui/react'
+import { Flex, Image } from '@chakra-ui/react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.svg'
+import { AuthContext } from '../context/AuthContext'
 
 const Logo = () => {
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <Flex justifyContent="center" alignItems="center">
-      <Link to="/">
+      <Link to={currentUser.role === 'ADMINISTRATOR' ? '/admin' : '/'}>
         <Image w="100px" src={logo} />
       </Link>
     </Flex>

90db9a9d980ab8fef3cd143f7211d1d835ef869d
Josel Catalan <catalanjosel123@gmail.com>
2023-05-09 09:50:31 +0800

    chore: set a default data if object is empty

--- src/utils/ColumnVisibilityToFilterData.js
+++ src/utils/ColumnVisibilityToFilterData.js
@@ -1,6 +1,29 @@
 import renameKeys from './renameKeys'
 
 const columnVisibilityToFilterData = (obj) => {
+  if (Object.keys(obj).length === 0) {
+    const defaultObj = {
+      isticketid: '1',
+      issubject: '1',
+      isconcern: '1',
+      isissue: '1',
+      isrequestername: '1',
+      isrequesteremail: '1',
+      isdescription: '1',
+      ispriority: '1',
+      isticketstatus: '1',
+      isdatecreated: '1',
+      isduedate: '1',
+      isstatusdetail: '1',
+      isassignto: '1',
+      isdepartment: '1',
+      isattachement: '1',
+      iscomment: '1',
+    }
+
+    return defaultObj
+  }
+
   const newKeys = {
     ticketid: 'isticketid',
     subject: 'issubject',

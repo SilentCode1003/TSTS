819f2b2ae17ec65085f5af99dd948d544e3a9c86
Josel Catalan <catalanjosel123@gmail.com>
2023-05-12 09:25:37 +0800

    fix: fix attachment links

--- src/components/TicketViewTopCard.jsx
+++ src/components/TicketViewTopCard.jsx
@@ -15,21 +15,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { serializedDataToFile } from '../utils/fileData'
 
+const TicketAttachments = ({ searchedTicket }) => {
+  const files = searchedTicket.attachement.split(' 5LJOIN ')
+
+  return files.map((file) => {
+    const { fileName, fileData } = serializedDataToFile(file)
+
+    return (
+      <Link key={fileName} to={fileData} download={fileName}>
+        {fileName} <DownloadIcon color="blue" />
+      </Link>
+    )
+  })
+}
+
 const TicketViewTopCard = ({ searchedTicket }) => {
-  const attachments = () => {
-    const files = searchedTicket.attachement.split(' 5LJOIN ')
-
-    return files.map((file) => {
-      const { fileName, fileData } = serializedDataToFile(file)
-
-      return (
-        <Link key={fileName} href={fileData} download={fileName}>
-          {fileName} <DownloadIcon color="blue" />
-        </Link>
-      )
-    })
-  }
-
   return (
     <Card>
       <CardHeader>
@@ -76,7 +76,9 @@ const TicketViewTopCard = ({ searchedTicket }) => {
           <Grid templateColumns="1fr" gap="1">
             <Text as="b">Attachments</Text>
             <SimpleGrid columns={['1', null, '2']}>
-              {searchedTicket.attachement && attachments()}
+              {searchedTicket.attachement && (
+                <TicketAttachments searchedTicket={searchedTicket} />
+              )}
               {!searchedTicket.attachement && <Text>No attachments</Text>}
             </SimpleGrid>
           </Grid>

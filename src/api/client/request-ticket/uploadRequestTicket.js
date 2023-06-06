7e61c642fd6a8db7a011dda188dc726badb41ca6
Josel Catalan <catalanjosel123@gmail.com>
2023-06-06 13:51:47 +0800

    feat: create client queries for dashboard

---
+++ src/api/client/request-ticket/uploadRequestTicket.js
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query'
+import { axios } from '../../axios'
+import pako from 'pako'
+
+export const uploadRequestTicket = async (newTicket) => {
+  const compressedData = pako.gzip(JSON.stringify(newTicket))
+  const res = await axios.post('', compressedData, {
+    headers: {
+      'Content-Encoding': 'gzip',
+      'Content-Type': 'application/json',
+    },
+  })
+  return res.data
+}
+
+export const useUploadRequestTicket = () => {
+  return useMutation({
+    mutationKey: ['requestTicket'],
+    mutationFn: (newTicket) => uploadRequestTicket(newTicket),
+  })
+}

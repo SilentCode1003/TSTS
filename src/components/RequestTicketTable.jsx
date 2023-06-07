ee276c461c53898dcd742b5f90d07bba1d639985
Josel Catalan <catalanjosel123@gmail.com>
2023-06-07 13:51:26 +0800

    refactor: :^)

--- src/components/RequestTicketTable.jsx
+++ src/components/RequestTicketTable.jsx
@@ -30,7 +30,7 @@ const RequestTicketTable = () => {
   if (data.length <= 0) {
     return (
       <Text fontSize="sm" color="green.400">
-        No ticket requests :^)
+        No ticket requests :^&#41;
       </Text>
     )
   }

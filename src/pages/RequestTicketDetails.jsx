ac6a06652f98731611533fcad572779d8d734ed8
Josel Catalan <catalanjosel123@gmail.com>
2023-06-15 11:18:44 +0800

    feat: make gap larger on RequestTicketDetails card

--- src/pages/RequestTicketDetails.jsx
+++ src/pages/RequestTicketDetails.jsx
@@ -87,27 +87,27 @@ const RequestTicketDetails = () => {
               fontSize="sm"
               divider={<Divider />}
             >
-              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="1">
+              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="4">
                 <Text as="b">Request ID</Text>
                 <Text>{searchedTicket?.requestid}</Text>
               </Grid>
 
-              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="1">
+              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="4">
                 <Text as="b">Requested By</Text>
                 <Text>{searchedTicket?.requestby}</Text>
               </Grid>
 
-              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="1">
+              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="4">
                 <Text as="b">Date Requested</Text>
                 <Text>{searchedTicket?.requestdate}</Text>
               </Grid>
 
-              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="1">
+              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="4">
                 <Text as="b">Concern</Text>
                 <Text>{searchedTicket?.concern}</Text>
               </Grid>
 
-              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="1">
+              <Grid templateColumns={['1fr', null, '3fr 9fr']} gap="4">
                 <Text as="b">Issue</Text>
                 <Text>{searchedTicket?.issue}</Text>
               </Grid>

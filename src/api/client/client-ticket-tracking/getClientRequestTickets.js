bd4bc8c760e2995cbcc0e90acd9b3d5eefe90909
Josel Catalan <catalanjosel123@gmail.com>
2023-06-15 09:07:46 +0800

    feat: make ClientTicketTracking page functioning

--- src/api/client/client-ticket-tracking/getClientRequestTickets.js
+++ src/api/client/client-ticket-tracking/getClientRequestTickets.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { axios } from '../../axios'
 
 export const getClientRequestTickets = async (inputs) => {
-  const res = await axios.post('requestticket/getrequestticket', inputs)
+  const res = await axios.post('/requestticket/getrequestticket', inputs)
   return res.data
 }
 
